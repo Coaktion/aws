490060bd5974686ff36a2feb7376992070eb41fa
gustavo <gus.inacio@hotmail.com>
2023-07-21 17:19:07 -0300

    feat: aws sns types

---
+++ src/sns/types.ts
@@ -0,0 +1,29 @@
+import { type ClientOptions } from 'src/common/client-options'
+
+export interface SNSClientOptions extends ClientOptions {
+  endpoint?: string
+}
+
+export interface SNSPublishOptions {
+  topicArn: string
+  message: any
+  phoneNumber?: string
+  targetArn?: string
+  subject?: string
+  messageStructure?: string
+  messageAttributes?: any
+  messageGroupId?: string
+}
+
+export interface SNSBulkEntry {
+  subject?: string
+  message: any
+  messageStructure?: string
+  messageAttributes?: any
+  messageGroupId?: string
+}
+
+export interface SNSBulkPublishOptions {
+  topicArn: string
+  entries: SNSPublishOptions[]
+}
